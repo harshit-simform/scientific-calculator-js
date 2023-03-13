b9c97940759cce72addc7e4b7c4c5ead1e882129
harshit-simform <harshit.g@simformsolutions.com>
2023-03-13 14:24:03 +0530

    feat: :sparkles: Added basic calculator functionality and also  add some style in calc

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-let a = 10;
-function run() {
-  function fast() {
-    console.log(a);
+const screen = document.getElementById("screen");
+console.log(screen.value);
+console.log("hiii");
+const button = document.querySelectorAll(".btn");
+console.log(button);
+button.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    var btnText = btn.innerText;
+    switch (btnText) {
+      case "\u00F7":
+        btnText = "/";
+        break;
+      case "\u2A09":
+        btnText = "*";
+        break;
+      case "mod":
+        btnText = "%";
+        break;
+      case "xy":
+        btnText = "^";
+        break;
+    }
+    screen.value += btnText;
+    console.log(screen.value);
+  });
+});
+
+function finalAnswer(value) {
+  if (value.includes("^")) {
+    value = value.replace("^", "**");
   }
-  return fast;
+  screen.value = eval(value);
 }
-var x = run();
-x();
